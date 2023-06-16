76f20e97ad7bfc63621cab2c9ab0452f6638e106
Leonid Lebidko <leonid.lebidko@rtlabs.ru>
2023-06-16 18:13:49 +0300

    домашнее задание 3: перенос и настройка api

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,22 @@
 rootProject.name = "otus-kotlin"
 
 //include("m1l1-hello")
-include("ok-marketplace-acceptance")
+include("ok-regatta-acceptance")
+
+include("ok-regatta-api-v1-jackson")
+include("ok-regatta-api-v2-kmp")
+
+include("ok-regatta-common")
+include("ok-regatta-mappers-v1")
+include("ok-regatta-mappers-v2")
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
+
     plugins {
         kotlin("jvm") version kotlinVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
